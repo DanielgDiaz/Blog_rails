3eeddaf6d399a47986d894064b1b0cadede6ee40
DanielgDiaz <danielgdiaz92@gmail.com>
2022-06-06 17:09:48 +0200

    edit, update & destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,18 @@ class ArticlesController < ApplicationController
 
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+
+        @article.update(title: params[:article][:title], content: params[:article][:content]) 
+        
+        redirect_to @article
+    end
+
     def new
         @article = Article.new 
     end
@@ -12,5 +24,11 @@ class ArticlesController < ApplicationController
     def create 
       @article = Article.create(title: params[:article][:title], content: params[:article][:content]) 
       render json: @article
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to root_path
     end
 end
