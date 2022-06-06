4f879924dc8ae1a99465b10d8b00482c772ae3a0
DanielgDiaz <danielgdiaz92@gmail.com>
2022-06-06 17:28:27 +0200

    before action :find_article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,24 +1,19 @@
 class ArticlesController < ApplicationController
+    before_action :find_article, except: [:new,:create]
 
     def show
-        @article = Article.find(params[:id])
-
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def update
-        @article = Article.find(params[:id])
-
         @article.update(title: params[:article][:title], content: params[:article][:content]) 
         
         redirect_to @article
     end
 
     def new
-        @article = Article.new 
     end
 
     def create 
@@ -27,8 +22,11 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         redirect_to root_path
+    end
+
+    def find_article
+        @article = Article.find(params[:id])
     end
 end
